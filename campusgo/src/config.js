0bca14b8cbeb3b61a9ec91eed6e217a2ef5e15ac
Aisack23 <00016023@uca.edu.sv>
2025-10-30 20:57:08 -0600

    Fire base-design v7

--- campusgo/src/config.js
+++ campusgo/src/config.js
@@ -1,2 +1,11 @@
-export const eventStartDate = new Date("2025-11-26T11:30:00");
-export const postEventDate = new Date("2025-11-27T00:00:00");
+const defaultStartDate = "2025-11-26T11:30:00";
+const defaultPostDate = "2025-11-27T00:00:00";
+
+// Lee la variable de entorno O usa el valor por defecto
+export const eventStartDate = new Date(
+  process.env.REACT_APP_EVENT_START_DATE || defaultStartDate
+);
+
+export const postEventDate = new Date(
+  process.env.REACT_APP_POST_EVENT_DATE || defaultPostDate
+);
